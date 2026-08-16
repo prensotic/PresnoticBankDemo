bd174698397eb04f31b2cabb0c2706f461a2a509
presnotic <vhxhje3ouxg3ofo@gmail.com>
2026-08-16 17:00:22 +0300

    Push to git

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,8 +2,11 @@ package app;
 
 import core.Bank;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
 public class Main {
-    static void main() {
+    static void main() throws NoSuchAlgorithmException, InvalidKeySpecException {
         Bank bank = new Bank();
         bank.test();
         bank.start();
